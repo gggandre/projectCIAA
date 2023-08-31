e0880b142d6ee2edb621f384369e7c0dc13ceac1
Gilberto André García Gaytán <a01753176@itesm.mx>
2023-08-31 17:32:29 -0600

    modelo y post

--- servidor.py
+++ servidor.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
+import numpy as np
+from joblib import load
+import os
+
+# Cargar el modelo
+dt=load('data\dt1.joblib')
 
 # Generar el servidor (Backend)
 servidorWeb = Flask(__name__)
@@ -6,6 +12,15 @@ servidorWeb = Flask(__name__)
 @servidorWeb.route("/holamundo", methods=['GET'])
 def holamundo():
         return render_template('pagina1.html')
+
+# Envío de datos a través de JSON
+@servidorWeb.route("/modelo", methods=['POST'])
+def modeloPrediccion():
+        # Procesarlos datos de entrada
+        contenido = request.json
+        print(contenido)
+        return jsonify({'resultado': "Hola"})
+
 
 if __name__ == '__main__':
         servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
