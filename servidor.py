7091570b888810ef14a26f87315238adea6fd6d5
Gilberto André García Gaytán <a01753176@itesm.mx>
2023-08-31 16:15:24 -0600

    servidor y html

---
+++ servidor.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+
+# Generar el servidor (Backend)
+servidorWeb = Flask(__name__)
+
+@servidorWeb.route("/holamundo", methods=['GET'])
+def holamundo():
+        return render_template('pagina1.html')
+
+if __name__ == '__main__':
+        servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
