4da46f90d385c4dbc6ad295f3029f9c2fc341a72
Gilberto André García Gaytán <a01753176@itesm.mx>
2023-09-06 19:49:49 -0600

    form

--- servidor.py
+++ servidor.py
@@ -32,7 +32,7 @@ def modeloPrediccion():
 @servidorWeb.route("/modeloForm", methods=['POST'])
 def modeloForm():
         # Procesarlos datos de entrada
-        contenido = request.json
+        contenido = request.form
         print(contenido)
         datosEntrada = np.array([ 0.88, 0, 2.6, 0.098, 25, 67, 0.9968, 1, 0.4,
                 contenido['pH'],
