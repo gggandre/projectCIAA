60a4beff6b3ecc7984d4edc473b17ceed65916db
Gilberto André García Gaytán <a01753176@itesm.mx>
2023-09-06 19:49:10 -0600

    a

--- servidor.py
+++ servidor.py
@@ -27,12 +27,12 @@ def modeloPrediccion():
         resultado = dt.predict(datosEntrada.reshape(1, -1))
         # Enviar el resultado
         return jsonify({'resultado': str(resultado[0])})
+        # return jsonify({'resultado': "Hola"})
 
-# Envío de datos a través de JSON
 @servidorWeb.route("/modeloForm", methods=['POST'])
 def modeloForm():
         # Procesarlos datos de entrada
-        contenido = request.form
+        contenido = request.json
         print(contenido)
         datosEntrada = np.array([ 0.88, 0, 2.6, 0.098, 25, 67, 0.9968, 1, 0.4,
                 contenido['pH'],
@@ -42,7 +42,7 @@ def modeloForm():
         resultado = dt.predict(datosEntrada.reshape(1, -1))
         # Enviar el resultado
         return jsonify({'resultado': str(resultado[0])})
-
+        # return jsonify({'resultado': "Hola"})
 
 if __name__ == '__main__':
         servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
