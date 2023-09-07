2597e6d4d7cf0eec68086ef4e5b5b3a4794a8f63
Gilberto André García Gaytán <a01753176@itesm.mx>
2023-09-06 19:37:05 -0600

    form

--- servidor.py
+++ servidor.py
@@ -10,12 +10,12 @@ dt=load('model_ml/dt1.joblib')
 servidorWeb = Flask(__name__)
 
 @servidorWeb.route("/holamundo", methods=['GET'])
-def holamundo():
+def modeloForm():
         return render_template('pagina1.html')
 
 # Envío de datos a través de JSON
 @servidorWeb.route("/modelo", methods=['POST'])
-def modeloPrediccion():
+def modeloForm():
         # Procesarlos datos de entrada
         contenido = request.json
         print(contenido)
@@ -27,7 +27,21 @@ def modeloPrediccion():
         resultado = dt.predict(datosEntrada.reshape(1, -1))
         # Enviar el resultado
         return jsonify({'resultado': str(resultado[0])})
-        # return jsonify({'resultado': "Hola"})
+
+# Envío de datos a través de JSON
+@servidorWeb.route("/modeloForm", methods=['POST'])
+def modeloForm():
+        # Procesarlos datos de entrada
+        contenido = request.form
+        print(contenido)
+        datosEntrada = np.array([ 0.88, 0, 2.6, 0.098, 25, 67, 0.9968, 1, 0.4,
+                contenido['pH'],
+                contenido['sulphates'],
+                contenido['alcohol']])
+        # Utulizar el modelo para hacer la predicción
+        resultado = dt.predict(datosEntrada.reshape(1, -1))
+        # Enviar el resultado
+        return jsonify({'resultado': str(resultado[0])})
 
 if __name__ == '__main__':
         servidorWeb.run(debug=False, host='0.0.0.0', port='8080')
