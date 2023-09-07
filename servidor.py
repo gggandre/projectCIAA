d4c8fcb4ee4713b0e6eabb2f1f09779f590e9538
Gilberto André García Gaytán <a01753176@itesm.mx>
2023-09-06 19:13:23 -0600

    change

--- servidor.py
+++ servidor.py
@@ -19,7 +19,7 @@ def modeloPrediccion():
         # Procesarlos datos de entrada
         contenido = request.json
         print(contenido)
-        datosEntrada = np.array([ 0.88, 0, 2.6, 0.098, 25, 67, 0.9968, 1, 0.4
+        datosEntrada = np.array([ 0.88, 0, 2.6, 0.098, 25, 67, 0.9968, 1, 0.4,
                 contenido['pH'],
                 contenido['sulphates'],
                 contenido['alcohol']])
